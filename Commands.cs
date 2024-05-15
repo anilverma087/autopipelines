ff176961ec30caa9a37b445d4ba259ade0a6cd61
Anil Verma <anilverma087@gmail.com>
2024-05-15 16:42:39 +0530

    Committing files

---
+++ Commands.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows.Input;
+
+namespace AutoPipeline
+{
+    public class FileCopyCommand : ICommand
+    {
+        private readonly string _sourceFilePath;
+        private readonly string _destinationFilePath;
+        public FileCopyCommand(string sourceFilePath, string destinationFilePath)
+        {
+            _sourceFilePath = sourceFilePath;
+            _destinationFilePath = destinationFilePath;
+        }
+        public void ExecuteCommand()
+        {
+            try
+            {
+                // Check if source file exists
+                if (!File.Exists(_sourceFilePath))
+                {
+                    Console.WriteLine($"Source file : '{_sourceFilePath}' does not exist.");
+                    return;
+                }
+
+                // Check if destination directory exists, create it if not
+                string destinationDirectory = Path.GetDirectoryName(_destinationFilePath);
+                if (!Directory.Exists(destinationDirectory))
+                {
+                    Directory.CreateDirectory(destinationDirectory);
+                }
+
+                // Copy the file
+                File.Copy(_sourceFilePath, _destinationFilePath, true); // Overwrite if the file exists
+                Console.WriteLine($"File copied from '{_sourceFilePath}' to '{_destinationFilePath}'.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error copying file: {ex.Message}");
+            }
+        }
+    }
+    public class DeleteFileCommand : ICommand
+    {
+        private readonly string _filePath;
+        public DeleteFileCommand(string filePath)
+        {
+            _filePath = filePath;
+        }
+        public void ExecuteCommand()
+        {
+            try
+            {
+                // Check if the file exists
+                if (File.Exists(_filePath))
+                {
+                    // Delete the file
+                    File.Delete(_filePath);
+                    Console.WriteLine($"File : '{_filePath}' deleted successfully!");
+                }
+                else
+                {
+                    Console.WriteLine($"File : '{_filePath}' not found.");
+                }
+               
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error while deleting file: {ex.Message}");
+            }
+        }
+    }
+    public class CreateFolderCommand : ICommand
+    {
+        private readonly string _folderPath;
+        private readonly string _folderName;
+
+        public CreateFolderCommand(string folderPath, string folderName)
+        {
+            _folderPath = folderPath;
+            _folderName = folderName;
+        }
+        public void ExecuteCommand()
+        {
+            try
+            {
+                string fullFolderPath = Path.Combine(_folderPath, _folderName);
+                if (Directory.Exists(fullFolderPath))
+                {
+                    Console.WriteLine($"Folder '{fullFolderPath}' already exists.");
+                }
+                else
+                {
+                    // Create new folder
+                    Directory.CreateDirectory(fullFolderPath);
+                    Console.WriteLine($"Folder '{fullFolderPath}' created successfully.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error while creating folder: {ex.Message}");
+            }
+        }
+    }
+    public class GetAllFilesCommand : ICommand
+    {
+        private readonly string _folderPath;
+
+        public GetAllFilesCommand(string folderPath)
+        {
+            _folderPath = folderPath;
+        }
+        public void ExecuteCommand()
+        {
+            try
+            {
+                if (!Directory.Exists(_folderPath))
+                {
+                    Console.WriteLine($"Folder '{_folderPath}' does not exists");
+                }
+                string[] files = Directory.GetFiles(_folderPath);
+                Console.WriteLine($"Files in the folder : '{_folderPath}': Total No. of files : {files.Length} ");
+                foreach (string file in files)
+                {
+                    Console.WriteLine(file);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error while creating folder: {ex.Message}");
+            }
+        }
+    }
+    public class DownloadFileCommand : ICommand
+    {
+        private readonly string _filePath;
+
+        public DownloadFileCommand(string filePath)
+        {
+            _filePath = filePath;
+        }
+        public void ExecuteCommand()
+        {
+            try
+            {
+                string downloadFolderPath = "C:\\Downloads";  // created download folder if not exists
+                if (!Directory.Exists(downloadFolderPath))
+                {
+                    Directory.CreateDirectory(downloadFolderPath); // created download folder if not exists
+                }
+                string fileName = Path.GetFileName(_filePath);
+                string destinationFilePath = Path.Combine(downloadFolderPath, fileName);
+                File.Copy(_filePath, destinationFilePath, true); // set true to Overwrite if the file exists.
+                Console.WriteLine($"File '{fileName}' downloaded to '{downloadFolderPath}'.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error while downloading the file: {ex.Message}");
+            }
+        }
+
+    }
+    public class WaitCommand : ICommand
+    {
+        private readonly string _timeInSeconds;
+        public WaitCommand(string timeInSeconds)
+        {
+            _timeInSeconds = timeInSeconds;
+        }
+        public void ExecuteCommand()
+        {
+            try
+            {
+                int seconds = Convert.ToInt32(_timeInSeconds);
+                Console.WriteLine($"Please wait for {seconds} seconds...");
+                DateTime startTime = DateTime.Now;
+                DateTime endTime = startTime.AddSeconds(seconds);
+                int lastPrintedSecond = 0;
+                while (DateTime.Now < endTime)
+                {
+                    int currentSecond = DateTime.Now.Second;
+                    if (currentSecond != lastPrintedSecond) 
+                    {
+                        int remainingTime = (int)Math.Ceiling((endTime - DateTime.Now).TotalSeconds);
+                        Console.WriteLine($"Remaining time: {remainingTime} seconds");  // print only when the second changes.
+                        lastPrintedSecond = currentSecond;
+                    }
+                }
+
+                Console.WriteLine("Wait completed...");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error while waiting: {ex.Message}");
+            }
+        }
+
+    }
+    public class searchTextInFileCommand : ICommand
+    {
+        private readonly string _filePath;
+        private readonly string _searchString;
+
+        public searchTextInFileCommand(string filePath, string searchString)
+        {
+            _filePath = filePath;
+            _searchString = searchString;
+        }
+        public void ExecuteCommand()
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines(_filePath);
+                int count = 0;
+                foreach (string line in lines)
+                {
+                    if (line.Contains(_searchString))
+                    {
+                        Console.WriteLine(line);
+                        count++;
+                    }
+                }
+                Console.WriteLine($"Number of rows containing '{_searchString}' in file '{_filePath}'  is, Total : {count}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error while counting rows in the file: {ex.Message}");
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Windows.Input;
+namespace AutoPipeline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            ICommand iCI = null;
+            while (true)
+            {
+                Console.WriteLine("Select any option from below");
+                Console.WriteLine("1. Type 1 to File Copy");
+                Console.WriteLine("2. Type 2 to File Delete");
+                Console.WriteLine("3. Type 3 to Query Folder Files");
+                Console.WriteLine("4. Type 4 to Create Folder");
+                Console.WriteLine("5. Type 5 to Download File");
+                Console.WriteLine("6. Type 6 to Wait");
+                Console.WriteLine("7. Type 7 to Conditional Count Rows File");
+                Console.Write("Enter the number of the command you want to execute:  ");
+                string command = Console.ReadLine();
+                ExecuteSelectedCommand(command, iCI);
+            
+            }
+        }
+
+
+        static void ExecuteSelectedCommand(string command, ICommand iCI)
+        {
+
+            switch (command)
+            {
+                case "1":
+                    Console.Write("Enter source file path with name eg:- (F:\\TestingData\\1.txt) :-  ");
+                    string sourceFilePath = Console.ReadLine();
+                    Console.Write("Enter destination file path with name eg:- (F:\\TestingData\\new.txt) :-  ");
+                    string destinationFilePath = Console.ReadLine();
+                    iCI = new FileCopyCommand(sourceFilePath, destinationFilePath);
+                    iCI.ExecuteCommand();
+                    break;
+                case "2":
+                    Console.Write("Enter  file path to delete eg:- (F:\\TestingData\\1.txt) :-  ");
+                    string filepath = Console.ReadLine();
+                    //iCE.deleteFile(filepath);
+                    iCI = new DeleteFileCommand(filepath);
+                    iCI.ExecuteCommand();
+                    break;
+                case "3":
+                    Console.Write("Enter folpder path to get all files list :-  ");
+                    string folderPath = Console.ReadLine();
+                    iCI = new GetAllFilesCommand(folderPath);
+                    iCI.ExecuteCommand();
+                    break;
+                case "4":
+                    Console.Write("Enter folpder path  :-  ");
+                    string folderpath = Console.ReadLine();
+                    Console.Write("Enter folpder Name :-  ");
+                    string foldername = Console.ReadLine();
+                    iCI = new CreateFolderCommand(folderpath, foldername);
+                    iCI.ExecuteCommand();
+                    break;
+                case "5":
+                    Console.Write("Enter file path to download eg:- (F:\\TestingData\\1.txt) :-  ");
+                    string downloadfilepath = Console.ReadLine();
+                    iCI = new DownloadFileCommand(downloadfilepath);
+                    iCI.ExecuteCommand();
+                    break;
+                case "6":
+                    Console.Write("Enter time in seconds to wait: ");
+                    string timeInSeconds = Console.ReadLine();
+                    iCI = new WaitCommand(timeInSeconds);
+                    iCI.ExecuteCommand();
+                    break;
+                case "7":
+                    Console.Write("Enter file path to read eg:- (F:\\TestingData\\1.txt) :-  ");
+                    string filePath = Console.ReadLine();
+                    Console.Write("Enter text to seacrh in file :- ");
+                    string searchText = Console.ReadLine();
+                    iCI = new searchTextInFileCommand(filePath, searchText);
+                    iCI.ExecuteCommand();
+                    break;
+                default:
+                    Console.WriteLine("Exiting...");
+                    break;
+            }
+
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine(); // for empty line
+        }
+
+
+        //static void ExecuteSelectedCommand(string command, ICommandInterface iCI)
+        //{
+
+        //    switch (command)
+        //    {
+        //        case "1":
+        //            Console.Write("Enter source file path with name eg:- (F:\\TestingData\\1.txt) :-  ");
+        //            string sourceFilePath = Console.ReadLine();
+        //            Console.Write("Enter destination file path with name eg:- (F:\\TestingData\\new.txt) :-  ");
+        //            string destinationFilePath = Console.ReadLine();
+        //            //iCE.copyFile(sourceFilePath, destinationFilePath);
+        //            iCI = new FileCopyCommand(sourceFilePath, destinationFilePath);
+        //            iCI.ExecuteCommand();
+        //            break;
+        //        case "2":
+        //            Console.Write("Enter  file path to delete :-  ");
+        //            string filepath = Console.ReadLine();
+        //            //iCE.deleteFile(filepath);
+        //            iCI = new DeleteFileCommand(filepath);
+        //            iCI.ExecuteCommand();
+        //            break;
+        //        case "3":
+        //            Console.Write("Enter folpder path to get all files list :-  ");
+        //            string folderPath = Console.ReadLine();
+        //            iCE.getAllFiles(folderPath);
+        //            break;
+        //        case "4":
+        //            Console.Write("Enter folpder path :-  ");
+        //            string folderpath = Console.ReadLine();
+        //            Console.Write("Enter folpder Name :-  ");
+        //            string foldername = Console.ReadLine();
+        //            iCE.createFolder(folderpath, foldername);
+        //            break;
+        //        case "5":
+        //            Console.Write("Enter file path to download eg:- (F:\\TestingData\\1.txt) :-  ");
+        //            string downloadfilepath = Console.ReadLine();
+        //            iCE.downloadFile(downloadfilepath);
+        //            break;
+        //        case "6":
+        //            Console.Write("Enter time in seconds to wait: ");
+        //            string timeInSeconds = Console.ReadLine();
+        //            iCE.waitExecution(timeInSeconds);
+        //            break;
+        //        case "7":
+        //            Console.Write("Enter file path to read eg:- (F:\\TestingData\\1.txt) :-  ");
+        //            string filePath = Console.ReadLine();
+        //            Console.Write("Enter text to seacrh in file :- ");
+        //            string searchText = Console.ReadLine();
+        //            iCE.searchTextInFile(filePath, searchText);
+        //            break;
+        //        default:
+        //            Console.WriteLine("Exiting...");
+        //            break;
+        //    }
+
+        //    Console.WriteLine("----------------------------------");
+        //    Console.WriteLine(); // for empty line
+        //}
+    }
+
+}
